7ad1d9c8562ff72504ed896bbe2226aa5b798abe
Garti <67469783+Szowek@users.noreply.github.com>
2024-04-21 16:24:45 +0200

    Included Sqlite and EntityFrameworkCore packages, added entity model for flight and appropriate DbContext

---
+++ FlightManagement.Database/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FlightManagement.Entities.ModelEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagement.Database.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
---
+++ FlightManagement.Entities/ModelEntities/Flight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagement.Entities.ModelEntities
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string FlightNumber { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string DepartureLocation { get; set; }
+        public string ArrivalLocation { get; set; }
+        public string AircraftType { get; set; }
+    }
+
+}
