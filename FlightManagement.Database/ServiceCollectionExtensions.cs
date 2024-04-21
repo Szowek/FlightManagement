f4195aacd223ef9076728077652ac940509b1142
Garti <67469783+Szowek@users.noreply.github.com>
2024-04-21 20:46:04 +0200

    Implemented sqlite database connection

---
+++ FlightManagement.Database/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FlightManagement.Database.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagement.Database
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite($"DataSource=FlightDatabase.db"));
+        }
+    }
+}
--- FlightManagement/Program.cs
+++ FlightManagement/Program.cs
@@ -1,8 +1,13 @@
+using FlightManagement.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDatabase(builder.Configuration);
 
 var app = builder.Build();
 
